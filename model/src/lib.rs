40442ee231f69f3dc30169b9697ae7e10a13075e
Ryan Pavlik <ryan.pavlik@gmail.com>
2022-12-29 16:34:01 -0600

    wip

--- model/src/lib.rs
+++ model/src/lib.rs
@@ -1,6 +1,27 @@
 // Copyright 2022, Ryan Pavlik <ryan@ryanpavlik.com>
 // SPDX-License-Identifier: GPL3+
 
+/// Person ID
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct PersonId(usize);
+
+/// Person info
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct PersonInfo {
+    person_id: PersonId,
+    person_name: String,
+}
+
+/// Prescription ID
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct RxId(usize);
+
+pub struct RxInfo {
+    rx_id: RxId,
+    person_id: PersonId,
+    rx_name: String,
+}
+
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub enum RefillPolicyTiming {
     /// May fill a given number of days after the most recent fill
